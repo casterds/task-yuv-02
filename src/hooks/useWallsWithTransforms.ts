868a7860c5d87f3094800dfaaa34d35717d35506
casterds <96631866+casterds@users.noreply.github.com>
2023-06-15 06:29:11 +0530

    Update useWallsWithTransforms.ts

--- src/hooks/useWallsWithTransforms.ts
+++ src/hooks/useWallsWithTransforms.ts
@@ -1,4 +1,4 @@
-import { rotate } from "../utils/rotate";
+import { rotate } from "../utils/rotate"; 
 import { round } from "../utils/round";
 import { wallCoords } from "../Maze/mapData";
 import { usePosition } from "./usePosition";
