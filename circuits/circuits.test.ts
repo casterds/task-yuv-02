4c8a170b8d3783d24aeeb810abf95b37da8b641e
casterds <96631866+casterds@users.noreply.github.com>
2023-06-15 06:07:39 +0530

    Update circuits.test.ts

--- circuits/circuits.test.ts
+++ circuits/circuits.test.ts
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require("path"); 
 const { F1Field, Scalar } = require("ffjavascript");
 exports.p = Scalar.fromString(
   "21888242871839275222246405745257275088548364400416034343698204186575808495617"
