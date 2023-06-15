58352cad5df792e7ffe12affdddfdc0108a75d53
casterds <96631866+casterds@users.noreply.github.com>
2023-06-15 06:28:14 +0530

    Update useContracts.ts

--- src/hooks/useContracts.ts
+++ src/hooks/useContracts.ts
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from "react"; 
 import { Contracts, ContractsContext } from "../providers/ContractsProvider";
 
 export const useContracts = (): Contracts => {
