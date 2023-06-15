3811b375a955a205be5d9274689a44549bce1444
casterds <96631866+casterds@users.noreply.github.com>
2023-06-15 06:27:00 +0530

    Update Web3Gate.tsx

--- src/routes/Web3Gate.tsx
+++ src/routes/Web3Gate.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from "react"; 
 import { ReactNode, useMemo } from "react";
 import { useQuerySigner } from "../api/useQuerySigner";
 import { WalletContext } from "../providers/WalletProvider";
