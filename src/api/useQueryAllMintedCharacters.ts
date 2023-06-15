6f27bb06840eaa446e900081c83d3a53998f3f57
casterds <96631866+casterds@users.noreply.github.com>
2023-06-15 06:36:27 +0530

    Update useQueryAllMintedCharacters.ts

--- src/api/useQueryAllMintedCharacters.ts
+++ src/api/useQueryAllMintedCharacters.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "react-query"; 
 import { FETCH_MINTED_CACHE_KEY, fetchAllMintedCharacters } from "./api";
 import { useQuerySigner } from "./useQuerySigner";
 import { useContracts } from "../hooks/useContracts";
