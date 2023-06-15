e3efeb8af5f1b10f0908b3d409a4015370062590
casterds <96631866+casterds@users.noreply.github.com>
2023-06-15 06:35:31 +0530

    Update FightScreen.tsx

--- src/Screens/FightScreen.tsx
+++ src/Screens/FightScreen.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from "react"; 
 import styled from "styled-components";
 import { Image } from "../components/Image";
 import { ButtonAttack } from "../components/Button";
