ffeeca208ba2362a10c9b53a1ddb004172ffe3cc
casterds <96631866+casterds@users.noreply.github.com>
2023-06-15 06:37:11 +0530

    Update useQueryPlayerStats.ts

--- src/api/useQueryPlayerStats.ts
+++ src/api/useQueryPlayerStats.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "react-query"; 
 import { GET_PLAYER_STATS_CACHE_KEY, getPlayerStats } from "./api";
 import { useQuerySigner } from "./useQuerySigner";
 import { useContracts } from "../hooks/useContracts";
